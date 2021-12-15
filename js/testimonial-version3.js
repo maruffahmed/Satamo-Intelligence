cf19534eadccaeeab7a63932f3de9d8378667e76
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2021-12-15 08:52:21 +0600

    Change home page

---
+++ js/testimonial-version3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+// testimonial slick slider
+$('.testimonial-list').slick({
+    arrows: true,
+    dots:false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true
+});
